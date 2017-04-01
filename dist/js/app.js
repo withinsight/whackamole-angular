82c8010e3ea4ebc95ab52485c7da6eaa0efec46e
Jeff Pelletier <jeff.pelletier@intridea.com>
2017-04-01 11:19:53 -0400

    Logic to show/hide moles

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,56 @@
-(function(){"use strict";angular.module("moleApp",[]).controller("MoleController",o);function o(){var o=this;o.score=0;o.moles=[{show:false},{show:false},{show:false},{show:false},{show:false},{show:false},{show:false},{show:false},{show:false}];o.moleClicked=e;s();function s(){}function e(){o.score=o.score+1}}})();
+(function() {
+  'use strict';
+
+  angular
+    .module('moleApp', [])
+    .controller('MoleController', MoleController);
+
+  function MoleController($timeout) {
+    var vm = this;
+
+    vm.score = 0;
+    vm.moles = [
+      {show: false},
+      {show: false},
+      {show: false},
+      {show: false},
+      {show: false},
+      {show: false},
+      {show: false},
+      {show: false},
+      {show: false}
+    ];
+    vm.moleClicked = moleClicked;
+    vm.popMole = popMole;
+    vm.popMoles = popMoles;
+
+    activate();
+
+    //////////
+
+    function activate() {
+      vm.popMoles();
+    }
+
+    function moleClicked() {
+      vm.score = vm.score + 1;
+    }
+
+    function popMole() {
+      var randomNum = Math.floor(Math.random() * (9 - 0)) + 0;
+
+      vm.moles[randomNum].show = true;
+      $timeout(function() {
+        vm.moles[randomNum].show = false;
+      }, 2000);
+    }
+
+    function popMoles() {
+      $timeout(function() {
+        vm.popMole();
+        vm.popMoles();
+      }, 2000);
+    }
+  }
+
+})();
--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@
     .module('moleApp', [])
     .controller('MoleController', MoleController);
 
-  function MoleController() {
+  function MoleController($timeout) {
     var vm = this;
 
     vm.score = 0;
@@ -21,17 +21,35 @@
       {show: false}
     ];
     vm.moleClicked = moleClicked;
+    vm.popMole = popMole;
+    vm.popMoles = popMoles;
 
     activate();
 
     //////////
 
     function activate() {
-
+      vm.popMoles();
     }
 
     function moleClicked() {
       vm.score = vm.score + 1;
+    }
+
+    function popMole() {
+      var randomNum = Math.floor(Math.random() * (9 - 0)) + 0;
+
+      vm.moles[randomNum].show = true;
+      $timeout(function() {
+        vm.moles[randomNum].show = false;
+      }, 2000);
+    }
+
+    function popMoles() {
+      $timeout(function() {
+        vm.popMole();
+        vm.popMoles();
+      }, 2000);
     }
   }
 
